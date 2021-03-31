4803569d843c816eb6b75583123b33970106a92a
pluulove <pluulove@gmail.com>
2021-03-31 20:40:26 +0900

    Remove unused import

--- app/src/main/java/com/pluu/util/lifecycleSimpleName.kt
+++ app/src/main/java/com/pluu/util/lifecycleSimpleName.kt
@@ -4,7 +4,6 @@ import android.app.Activity
 import android.app.Application
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
